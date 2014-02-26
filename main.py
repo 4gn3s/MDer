86b415151d08a401c2ef2938c9b47ecea7af9312
4gn3s <agnieszk1@gmail.com>
2014-02-26 16:42:15 +0100

    added bootstrap files, markdown to html conversion using markdown2

--- main.py
+++ main.py
@@ -1,7 +1,66 @@
+import os
+import subprocess
+import markdown2
+
 import traverser
 
-if __name__ == "__main__":
+default_html_dir = "./html"
+default_assets_dir = "./assets"
+
+date_format = '%A, %B %d, %Y'
+
+markdowner = markdown2.Markdown()
+
+
+def no_html(target):
+    if not os.path.exists(target):
+        return True
+    # check if .md file changed in the meantime?
+    return False
+
+
+def md_to_html(md):
+    print "generating html of", md
+    with open(md, 'r') as md_file:
+        md_content = md_file.read()
+        html = markdowner.convert(md_content)
+    return html
+
+
+def write_html(filename, html):
+    with open(filename, 'a') as t:
+        t.write(html)
+    print "saved as", filename
+
+
+def generate():
     files = traverser.traverse('.')
-    print "all files:", files
     md_files = traverser.filter_md(files)
-    print ".md:", md_files
+    print ".md files:", md_files
+    htmls = {}
+    if not os.path.exists(default_html_dir):
+        os.makedirs(default_html_dir)
+    for md in md_files:
+        html_filename = md[:-2] + 'html'
+        full_filename = default_html_dir + "/" + html_filename
+        if no_html(full_filename):
+            htmls[html_filename] = md_to_html(md)
+            write_html(full_filename, htmls[html_filename])
+        else:
+            print "file already exists"
+        print "changed %x files" % (len(htmls))
+
+
+def clean_all():
+    print "cleaning directory", default_html_dir
+    files = os.listdir(default_html_dir)
+    files = [default_html_dir + "/" + f for f in files]
+    n = len(files)
+    for f in files:
+        os.remove(f)
+    print "removed %x files" % (n)
+
+
+if __name__ == "__main__":
+    clean_all()
+    generate()
